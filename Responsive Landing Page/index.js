a89485cb637b806f98334118ee60ba5051692015
poluslg <prahladbiswas08@gmail.com>
2026-04-17 10:54:01 +0530

    update README, fix image path, and clean up code formatting

--- Responsive Landing Page/index.js
+++ Responsive Landing Page/index.js
@@ -1,6 +1,7 @@
 function toggleMenu() {
   const navLinks = document.getElementById("mobile-nav-link");
   navLinks.classList.toggle("active");
+  
 }
 
 document.querySelector(".menu-icon").addEventListener("click", toggleMenu);
